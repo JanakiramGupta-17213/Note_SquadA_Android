ef84e763ddaa08558f8c0d7d2805692c818bcc2e
Akhilmadishety <55476233+Akhilmadishety@users.noreply.github.com>
2021-10-14 00:33:44 -0400

    Updated NotesListAdapter package

--- app/src/main/java/com/example/notes_squada/Adapters/NotesListAdapter.java
+++ app/src/main/java/com/example/notes_squada/Adapters/NotesListAdapter.java
@@ -1,24 +1,66 @@
 package com.example.notes_squada.Adapters;
 
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NotesListAdapter extends RecyclerView.Adapter {
+import com.example.notes_squada.NotesDisplayActivity;
+import com.example.notes_squada.R;
+
+import java.util.Date;
+import java.util.List;
+
+public class NotesListAdapter extends RecyclerView.Adapter<NotesListAdapter.viewholder> {
+
+    List<String> notes_title;
+    List<Date> date;
+    public NotesListAdapter(List<String> notes_title, List<Date> date)
+    {
+        this.notes_title = notes_title;
+        this.date = date;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.notes_item,parent,false);
+        return new NotesListAdapter.viewholder(item);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull viewholder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return notes_title.size();
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder {
+        CardView tv_item;
+        TextView tv_itemtext, tv_itemsubtext;
+
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+            tv_itemtext = itemView.findViewById(R.id.tv_itemtext);
+            tv_itemsubtext = itemView.findViewById(R.id.tv_itemsubtext);
+            tv_item = itemView.findViewById(R.id.tv_item);
+
+            tv_item.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(itemView.getContext(), NotesDisplayActivity.class);
+
+                    itemView.getContext().startActivity(intent);
+                }
+            });
+        }
     }
 }
