3dc3af9cef1b0fda0903941ec83137d9d1b45069
Akhilmadishety <55476233+Akhilmadishety@users.noreply.github.com>
2021-10-14 00:43:07 -0400

    added logic and layout for NotesEntryActivity

--- app/src/main/java/com/example/notes_squada/NotesEntryActivity.java
+++ app/src/main/java/com/example/notes_squada/NotesEntryActivity.java
@@ -1,8 +1,10 @@
 package com.example.notes_squada;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class NotesEntryActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class NotesEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_entry);
+
+        getSupportActionBar().setTitle("Enter Notes");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
